19a8bf691e8f6ffde96762e5b1b621fed6ba29a0
dfsaker <dfsaker7@gmail.com>
2026-06-10 19:25:28 -0300

    datos

--- sensor_temperatura/sensor_temperatura.ino
+++ sensor_temperatura/sensor_temperatura.ino
@@ -3,10 +3,11 @@
 LCD_I2C lcd(0x27,20,4);
 
 
-// variable para guardar el numero de pin donde conectaremos el sensor LM35DZ
-int pinsensor = A1;
  
-#define BUZ 8
+#define BUZ 7
+int adc;
+float voltaje;
+float temperatura;
 
 // MATRIZ DE CARACTERES
 
@@ -48,11 +49,19 @@ void setup() {
 
 
   pinMode(BUZ,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(6,OUTPUT);
 
-  analogReference(INTERNAL);
+  digitalWrite(6,HIGH);
+  digitalWrite(8,HIGH);
+  
 
 
 
+
+  digitalWrite(BUZ,HIGH);
+  delay(500);
+   digitalWrite(BUZ,LOW);
  
 
   lcd.begin(20,4);
@@ -67,26 +76,24 @@ void setup() {
  
 }
  
-/**
-   Función loop: se ejecuta continuamente mientras el arduino permanece encendido
-*/
-void loop() {
 
-  // variable local para el valor del ADC que va en un rango de 0 a 1023
+void loop() 
+{
 
-  int adc = analogRead(pinsensor);
- 
-  // calculamos la temperatura con la fórmula siguiente
+            
 
-  float temperatura = (110.0 * adc) / 1024.0;
- 
+  adc = analogRead(A1);  // variable local para el valor del ADC que va en un rango de 0 a 1023
+
+  voltaje = (float) adc * 5.0 / 1023.0;  // CONVERSION DE ADC A VOLTAJE 
+  temperatura = voltaje * 1 / 0.01;  // calculamos la temperatura con la fórmula siguiente
+  
   // borrar toda la pantalla
 
-  lcd.clear();
+  //lcd.clear();
 
-  // imprimir a la pantalla una leyenda
+  // IMPRIME CARACTERES Y LEYENDA TEMP Y VOLT 
 
- 
+
   lcd.setCursor(0, 0);
   lcd.write(byte(0)); // Imprime el termómetro (espacio 0)
   lcd.print(" Temp:");  
@@ -96,66 +103,50 @@ void loop() {
   lcd.print(" Volt:"); 
   
 
-  // CONVERSION DE ADC A VOLTAJE 
-
-  float voltaje = (adc * 5.0) / 1023.0;
- 
-  delay(200);
+  //delay(10);
 
 
   
-  // imprimir el valor de la medición de temperatura
-
-  lcd.setCursor(7, 0); 
-
+  // IMPRIME VALOR DE TEMPERATURA 
+  lcd.setCursor(7, 0); // imprime en columna 7 y fila 0 
   lcd.print(temperatura, 1);
-
-  // secuencia de escape para imprimir el simbolo de grados que forma
-  // parte de los caracteres especiales de la pantalla LCD
-
   lcd.print(" \337C");
 
-  //IMPRIMIR VALOR DE VOLTAJE 
-  lcd.setCursor(7, 1);
+  //IMPRIME  VALOR DE VOLTAJE 
+  lcd.setCursor(7, 1); // imprime en columna 7 y fila 1 
   lcd.print(voltaje);
   lcd.print(" v");
- 
- 
-  
+
 
   
+  if (temperatura>40)   //CONDICION SI TEMPERATURA ES MAYOR A 40 GRADOS 
 
-
-//CONDICION SI TEMPERATURA ES MAYOR A 40 GRADOS 
-
-  if (temperatura>40)
-   {
-
-    lcd.setCursor(0,3);
-   // digitalWrite(BUZ,HIGH);
-    delay(500);
-    digitalWrite(BUZ,LOW);
-    int i;
- 
-    for (i=5;i>0;i--)
-        {
-          // Posicionamos y mandamos el texto centrado   lcd.setCursor(columnaInicio, fila);
+      {
       
-          lcd.setCursor(3,3);
-          lcd.print("Temp Maxima!!");
-          delay(250);
-          lcd.clear();
-          delay(250);
-          digitalWrite(BUZ, HIGH);
-          delay(250);
-          digitalWrite(BUZ, LOW);
-        }
- 
-    
-          
-   }
- 
+        int i;
+        
+        //for (i=5;i>0;i--)
+      
+        // lcd.clear();
+        lcd.setCursor(3,3);
+        lcd.print("Temp Maxima!!");
+        digitalWrite(BUZ, HIGH);
+        delay(300);
+       
+        digitalWrite(BUZ, LOW);
+        lcd.setCursor(3,3);
+        lcd.print("             ");
+       
+              
+            
+      }   
+
+                  
   // esperamos medio segundo  antes de realizar una nueva medición
-  // podemos personalizar este retardo a nuestro gusto
-  delay(500);
-}
+
+ delay(500);         
+ }             
+                    
+            
+          
+           
